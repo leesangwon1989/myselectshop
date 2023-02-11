1f84bcda9d50efe035e55445680c65e6db402535
snagwon <ezlsw@naver.com>
2023-02-11 17:10:50 +0900

    Project MySelectShop - JPA
    페이징 및 정렬

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -3,8 +3,10 @@ package com.sparta.myselectshop.controller;
 import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,9 +28,15 @@ public class ProductController {
 
     // 관심 상품 조회하기
     @GetMapping("/products")
-    public List<ProductResponseDto> getProducts(HttpServletRequest request) {
+    public Page<Product> getProducts(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc,
+            HttpServletRequest request
+    ) {
         // 응답 보내기
-        return productService.getProducts(request);
+        return productService.getProducts(request, page-1, size, sortBy, isAsc);
     }
 
     // 관심 상품 최저가 등록하기
